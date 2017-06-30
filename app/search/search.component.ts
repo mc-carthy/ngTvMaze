e4cfa624fa355218892a3b004fc2c0d97ef12e1a
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-06-30 16:10:26 +0100

    Implement searching of shows with TvMaze API

--- app/search/search.component.ts
+++ app/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { SearchService } from './search.service';
 import { Show } from './show';
 
 @Component({
@@ -7,8 +9,41 @@ import { Show } from './show';
 })
 
 export class SearchComponent implements OnInit {
-    
-    constructor() { }
 
-    ngOnInit() { }
+    searchForm: FormGroup;
+    shows: Show[];
+    isLoading: boolean;
+    errorMessage: string;
+
+    constructor(
+        private fromBuilder: FormBuilder,
+        private searchService: SearchService
+    ) { }
+
+    ngOnInit() {
+        this.searchForm = this.fromBuilder.group({
+            query: ['', [Validators.required, Validators.minLength(2)]]
+        });
+    }
+
+    searchShows({ value }: { value: any }) {
+        console.log(value.query);
+        this.isLoading = true;
+        this.resetSearch();
+        this.searchService.search(value.query)
+            .subscribe(shows => {
+                console.log(shows);
+                this.isLoading = false;
+                this.shows = shows;
+            },
+            err => this.errorMessage = <any> err
+            );
+    }
+
+    resetSearch()
+    {
+        this.errorMessage = null;
+        this.shows = null;
+        this.searchForm.reset();
+    }
 }
--- app/search/search.service.ts
+++ app/search/search.service.ts
@@ -1,11 +1,30 @@
 import { Injectable } from '@angular/core';
 import { Http, Response, URLSearchParams } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 import { Show } from './show';
 
 @Injectable()
 export class SearchService {
 
+    private tvMazeUrl: string = 'https://api.tvmaze.com/search/shows';
+
     constructor(
-        private http: Http;
+        private http: Http
     ) { }
+
+    search(query: any): Observable<Show[]>
+    {
+        let search: URLSearchParams = new URLSearchParams();
+        search.set('q', query);
+        return this.http.get(this.tvMazeUrl, { search })
+            .map((res: Response) => <Show[]> res.json())
+            .do(data => console.log(data))
+            .catch(this.handleError);
+    }
+
+    private handleError(error: Response)
+    {
+        let msg = `Error status code ${error.status} status ${error.statusText} at ${error.url}`;
+        return Observable.throw(msg);
+    }
 }
